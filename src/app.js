b705d894bea94a23a2f37357a0c5254ea828b66f
LanaKolisnyk <svkolesnik@gmail.com>
2022-08-24 21:42:36 +0300

    Added JS daily forecast and API forecast

--- src/app.js
+++ src/app.js
@@ -26,6 +26,38 @@ let today = document.querySelector("#today");
 today.innerHTML = `${formatDate(date)}`;
 
 let celsiusTemperature = null;
+
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row"`;
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+  <div class="col-2">
+    <div class="WeatherForecastPreview">
+      <div class="forecast-time">${day}</div>
+     
+      <div class="forecast-temperature">
+        <span class="forecast-temperature-max">28°</span
+        ><span class="forecast-temperature-min">22°</span>
+      </div>
+    </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "0593e3b15bfef2927adfa7fe26b2d02e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function cityWeather(response) {
   console.log(response.data);
 
@@ -46,6 +78,8 @@ function cityWeather(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 let apiKey = "0593e3b15bfef2927adfa7fe26b2d02e";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kiev&appid=${apiKey}&units=metric`;
