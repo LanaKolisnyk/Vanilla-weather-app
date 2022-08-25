5ac78d1bbd7f4373f7f7dfb3335d0826bc591877
LanaKolisnyk <svkolesnik@gmail.com>
2022-08-25 20:35:32 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -64,15 +64,12 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "0593e3b15bfef2927adfa7fe26b2d02e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
 function cityWeather(response) {
-  console.log(response.data);
-
   let location = document.querySelector("#location");
   location.innerHTML = response.data.name;
   let tempNow = document.querySelector("#tempnow");
