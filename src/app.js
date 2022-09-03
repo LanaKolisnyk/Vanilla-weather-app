9ef005f2bd5c5a679d5aa1bc153442c211903bb1
LanaKolisnyk <svkolesnik@gmail.com>
2022-09-03 20:41:49 +0300

    Added responsive grids

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ function displayForecast(response) {
       forecastHTML =
         forecastHTML +
         `
-  <div class="col-2">
+  <div class="col-sm-2">
           <div class="forecast-time">${formatDay(forecastDay.dt)}</div>
               <img src="http://openweathermap.org/img/wn/${
                 forecastDay.weather[0].icon
